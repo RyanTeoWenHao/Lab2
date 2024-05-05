960447bf19f811db1025e2b3dec29c0f950f0be4
RyanTeoWenHao <ryanteo.23@ichat.sp.edu.sg>
2024-05-06 00:37:22 +0800

    lab3

--- bmi.py
+++ bmi.py
@@ -5,10 +5,15 @@ def calculate_bmi(height,weight):
     print("BMI="+str(bmi))
     if bmi<18.5:
         print("Under Weight")
+        return -1
     else:
          if bmi>=18.5 and bmi<=25.0:
             print ("Normal Weight")
+            return 0
          else: 
             print("Over Weight")
+            return 1
 
 calculate_bmi(weight=57,height=1.73)
+
+
